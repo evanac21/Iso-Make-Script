1c64291c251d37a144029ba04e7f0a3d78236661
Plaligraphy <evanac21@gmail.com>
2019-03-29 07:22:00 -0700

    Add

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Iso-Make-Script
 Scripts to make the ISO from the main directory
+
+(CAN ONLY BE USED ON DEV COMPUTER)
---
+++ makeiso.sh
@@ -0,0 +1,5 @@
+
+sudo make -C archiso install
+echo "Complete"
+cd /home/dev/Projects/inprogress/XdsOS/archiso/configs/XdsOS-Base/
+sudo ./build.sh -v
